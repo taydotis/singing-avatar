00dae523f6026280a96e57aca07a7d5309c5b19c
Taylor Raboin <hello@tay.is>
2017-12-07 14:43:35 -0600

    Added Unity Project Files

---
+++ Unity/Assets/songscript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class songscript : MonoBehaviour {
+
+	public void playAudio () {
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+	}
+	
+}
